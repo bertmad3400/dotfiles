896513cdfda500000c5dfa5458da58b132a91811
Bertram Madsen <skrivtilbertram@gmail.com>
2026-05-20 13:37:49 +0200

    Fix disk and ram sketchybar plugins

--- .config/sketchybar/plugins/ram.sh
+++ .config/sketchybar/plugins/ram.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-sketchybar --set $NAME label="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%01.0f\n", 100-$5"%") }')%"
+PERCENT=$(vm_stat | awk '
+  /page size of/        { page = $8 + 0 }
+  /Pages active/        { active = $3 + 0 }
+  /Pages wired down/    { wired = $4 + 0 }
+  /Pages occupied by compressor/ { comp = $5 + 0 }
+  /Pages free/          { free = $3 + 0 }
+  /Pages inactive/      { inactive = $3 + 0 }
+  /Pages speculative/   { spec = $3 + 0 }
+  /Pages purgeable/     { purge = $3 + 0 }
+  END {
+    used  = active + wired + comp - purge
+    total = active + wired + comp + free + inactive + spec
+    if (total > 0) printf("%.0f", used * 100 / total)
+  }')
+
+sketchybar --set "$NAME" label="${PERCENT}%"
